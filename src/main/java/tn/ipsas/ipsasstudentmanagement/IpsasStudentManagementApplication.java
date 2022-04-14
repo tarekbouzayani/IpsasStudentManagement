df8a5be6251a4926b1b9a3e80cbaa2b4e3bce706
ASUS <tarek.bouzayani@gmail.com>
2022-04-14 20:07:52 +0100

    Initial commit

---
+++ src/main/java/tn/ipsas/ipsasstudentmanagement/IpsasStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package tn.ipsas.ipsasstudentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpsasStudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpsasStudentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/ipsas/ipsasstudentmanagement/IpsasStudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.ipsas.ipsasstudentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsasStudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
